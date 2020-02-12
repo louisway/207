443af8c42a52d543945051bf8402af16d6df6e76
louisway <lw2465@gmail.com>
2020-02-12 15:58:03 -0800

    add echo_server

---
+++ UNP/src/echo_server.c
@@ -0,0 +1,33 @@
+#include "unp_mac.h"
+#include <time.h>
+
+void str_echo(int _input){
+ printf("%d \n", _input); 
+}
+
+int main(int argc, char **argv){
+  int listenfd, connfd;
+  pid_t childpid;
+  socklen_t clilen;
+  struct sockaddr_in cliaddr, servaddr;
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);/* Create Socket, AF_INET:IPV4*/
+  bzero(&servaddr, sizeof(servaddr));/* reset servaddr to zero */
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);/* htonl host to network byte ; INADDR_ANY means the ip is not specified*/
+  servaddr.sin_port = htons(SERV_PORT);
+  bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+  listen(listenfd, LISTENQ);
+
+  for (;;) {
+    clilen = sizeof(cliaddr);
+    printf("Listening\n");
+    connfd = accept(listenfd, (SA*) &cliaddr, &clilen);
+    
+    if( (childpid = fork()) == 0){
+       close(listenfd);
+       str_echo(connfd);
+       exit(0); 
+    } 
+    close(connfd);
+  }
+}
