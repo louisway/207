60c84107d3bf07c66d82bb22169ac3b0379c19f7
louisway <lw2465@gmail.com>
2020-02-12 15:57:06 -0800

    normal modify

--- UNP/include/unp_mac.h
+++ UNP/include/unp_mac.h
@@ -5,6 +5,27 @@
 #define __unp_h
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "config.h"
+#include <arpa/inet.h>
+
+#include <sys/types.h>
+
+/*Socket()*/
+#include <sys/socket.h>
+
+/* pid_t fork exec */
+#include <unistd.h>
+
+/* sockaddr_in */
+#include <netinet/in.h>
+
+/* bzero */
+#include <strings.h>
+
+#define SERV_PORT  9877
+#define SERV_PORT_STR "9877"
+#define LISTENQ 1024 /* maximum number of client connection */
+#define SA struct sockaddr
 
 #endif
--- UNP/src/function_test.c
+++ UNP/src/function_test.c
@@ -1,15 +1,42 @@
 #include  "unp_mac.h"
+
+/* verified */
 #include <arpa/inet.h>
+
+
+/* verified */
+#include <sys/socket.h>
+
+/* verified */
+#include <unistd.h>
+
+/* verified */
+#include <netinet/in.h>
+
+/* verified */
+#include <strings.h>
+
 /**
   TEST PROGRAM for some functions
 **/
+
 void test_inet_aton(){
+/*
+  verified
+*/
   char * input = "206.168.112.96";
   struct in_addr nsaddr;
   int output = inet_aton(input,&nsaddr);
   printf("%ul\n",nsaddr.s_addr);
 }
 
+void test_bzero_strings(){
+/* verified */
+  struct sockaddr_in servaddr;
+  bzero(&servaddr,sizeof(servaddr));
+}
+
 int main(int argc, char **argv){
   test_inet_aton();
+  test_bzero_strings();
 }
